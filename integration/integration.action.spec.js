3e6f23902af615e4fa92ba1b6438e08e9b475fb6
Josh Cunningham <josh@joshcanhelp.com>
2022-05-20 12:02:47 -0700

    "feat: update README, GitHub Actions, and specs"

--- integration/integration.action.spec.js
+++ integration/integration.action.spec.js
@@ -1,18 +1,18 @@
-const { eventMock } = require("../__mocks__/event-post-password-change");
+const { makeEventMock } = require("../__mocks__/event-post-password-change");
 
 const { onExecutePostChangePassword } = require("./integration.action");
 
 describe("Action integration", () => {
-  let consoleLogMock;
+  let consoleLogMock, eventMock;
 
   beforeEach(() => {
     consoleLogMock = jest.spyOn(console, "log").mockImplementation();
+    eventMock = makeEventMock();
   });
 
   afterEach(() => {
     consoleLogMock.mockRestore();
     jest.clearAllMocks();
-    eventMock.secrets = {};
   });
 
   describe("onExecutePostChangePassword", () => {
